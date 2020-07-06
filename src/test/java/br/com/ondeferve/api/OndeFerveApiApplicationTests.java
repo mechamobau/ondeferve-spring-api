090a6131c93a0387a471c5da0e2e3f4a467144e0
Lucas Viana <vianadossantoslucas@gmail.com>
2020-07-06 20:35:16 -0300

    :tada: initial commit

---
+++ src/test/java/br/com/ondeferve/api/OndeFerveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ondeferve.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndeFerveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
