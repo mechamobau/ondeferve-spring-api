8ad180c0fecad60a4e6b8dd263687a200926e35d
jhonnatthan <jhonnatthans@outlook.com>
2020-07-07 18:52:08 -0300

    :sparkles: add JWT

--- src/main/java/br/com/ondeferve/api/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/br/com/ondeferve/api/config/JwtAuthenticationEntryPoint.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
-    private static final long serialVersionUID = -7858869558953243875L;
+    private static final long serialVersionUID = 1L;
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
