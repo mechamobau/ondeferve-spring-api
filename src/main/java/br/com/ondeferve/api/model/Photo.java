7e3560e99470482d4cf1e1f41553b32c85d647d5
jhonnatthan <jhonnatthans@outlook.com>
2020-07-07 00:40:18 -0300

    :bug: confirmation fix

--- src/main/java/br/com/ondeferve/api/model/Photo.java
+++ src/main/java/br/com/ondeferve/api/model/Photo.java
@@ -5,17 +5,9 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import br.com.ondeferve.api.model.Event;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 @Table(name = "photo")
@@ -23,12 +15,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Photo extends AbstractEntity {
     private static final long serialVersionUID = 1L;
 
-    @Column(name = "url", length = 255)
-    private String url;
 
     @OneToMany(cascade = CascadeType.ALL, 
 			   mappedBy = "photo")	
     private List<Event> events;
+    
+
+    @Column(name = "url", length = 255)
+    private String url;
+
     
 
     public Photo() {
